ebfa1e7d5c1ac339d7c0202d1adcee2893ed9f13
Nijat Mukhtarov <mukhog97@icloud.com>
2022-08-09 04:13:58 +0300

    bugfixes

--- assets/js/map.js
+++ assets/js/map.js
@@ -3,7 +3,7 @@ var queryString = decodeURIComponent(window.location.search);
 queryString = queryString.substring(1);
 var queries = queryString.split("&");
 var split = queries[0].split("=");
-
+var globalCity;
     const Vana = [43.2141,27.9147] 
     const Baku = [40.37767,49.867092] 
 
@@ -17,16 +17,14 @@ var split = queries[0].split("=");
 
     const Vanabeachnames = ["Varna beach","Sahara beach","Trakata beach","Rappongi Beach","Fichoza Beach Veteran","Beach Golden Sands","Asparuhovo beach","Albena Beach","Nirvana Beach","Noi Beach" ]
     
-    const Bakubeachinfo =["Wave Height: 1.2m</br> Temperature: 22C</br> Cleanliness Level: OK","Wave Height: 1.2m</br> Temperature: 22C</br> Cleanliness Level: OK","Wave Height: 1.2m</br> Temperature: 22C</br> Cleanliness Level: OK","Wave Height: 1.2m</br> Temperature: 22C</br> Cleanliness Level: OK","Wave Height: 1.2m</br> Temperature: 22C</br> Cleanliness Level: OK","Wave Height: 1.2m</br> Temperature: 22C</br> Cleanliness Level: OK","Wave Height: 1.2m</br> Temperature: 22C</br> Cleanliness Level: OK","Wave Height: 1.2m</br> Temperature: 22C</br> Cleanliness Level: OK","Wave Height: 1.2m</br> Temperature: 22C</br> Cleanliness Level: OK","Wave Height: 1.2m</br> Temperature: 22C</br> Cleanliness Level: OK"]
-
-    const Vanabeachinfo =["Wave Height: 1.2m</br> Temperature: 22C</br> Cleanliness Level: OK","Wave Height: 1.2m</br> Temperature: 22C</br> Cleanliness Level: OK","Wave Height: 1.2m</br> Temperature: 22C</br> Cleanliness Level: OK","Wave Height: 1.2m</br> Temperature: 22C</br> Cleanliness Level: OK","Wave Height: 1.2m</br> Temperature: 22C</br> Cleanliness Level: OK","Wave Height: 1.2m</br> Temperature: 22C</br> Cleanliness Level: OK","Wave Height: 1.2m</br> Temperature: 22C</br> Cleanliness Level: OK","Wave Height: 1.2m</br> Temperature: 22C</br> Cleanliness Level: OK","Wave Height: 1.2m</br> Temperature: 22C</br> Cleanliness Level: OK","Wave Height: 1.2m</br> Temperature: 22C</br> Cleanliness Level: OK"]
+    const Bakubeachinfo =["Wave Height: 1.2m</br> Temperature: 22C</br> Cleanliness Level: OK","Wave Height: 0.6m</br> Temperature: 24C</br> Cleanliness Level: Good","Wave Height: 1.5m</br> Temperature: 21C</br> Cleanliness Level: Excellent","Wave Height: 4.2m</br> Temperature: 19C</br> Cleanliness Level: OK","Wave Height: 1.3m</br> Temperature: 21C</br> Cleanliness Level: OK","Wave Height: 0.7m</br> Temperature: 26C</br> Cleanliness Level: OK","Wave Height: 1.3m</br> Temperature: 28C</br> Cleanliness Level: OK","Wave Height: 1.3m</br> Temperature: 23C</br> Cleanliness Level: Good","Wave Height: 1.6m</br> Temperature: 23C</br> Cleanliness Level: OK","Wave Height: 1.3m</br> Temperature: 12C</br> Cleanliness Level: OK"]
+    const Vanabeachinfo =["Wave Height: 1.1m</br> Temperature: 22C</br> Cleanliness Level: Bad","Wave Height: 1.6m</br> Temperature: 12C</br> Cleanliness Level: OK","Wave Height: 0.9m</br> Temperature: 24C</br> Cleanliness Level: OK","Wave Height: 2.2m</br> Temperature: 23C</br> Cleanliness Level: Good","Wave Height: 1.5m</br> Temperature: 22C</br> Cleanliness Level: OK","Wave Height: 0.6m</br> Temperature: 23C</br> Cleanliness Level: OK","Wave Height: 1.1m</br> Temperature: 16C</br> Cleanliness Level: OK","Wave Height: 1.1m</br> Temperature: 25C</br> Cleanliness Level: OK","Wave Height: 1.4m</br> Temperature: 21C</br> Cleanliness Level: Bad","Wave Height: 0.9m</br> Temperature: 21C</br> Cleanliness Level: OK"]
 
 function SetMap(city){
-    
+    globalCity = city
     const myIcon = L.icon({
         iconUrl: 'test2_files/marker-icon.png',
         iconSize: [20, 30],
-        
     });
     
     if(city == "Baku"){
@@ -65,5 +63,24 @@ function SetMap(city){
                 marker.bindPopup("<b>"+Vanabeachnames[i]+"</b></br>"+Vanabeachinfo[i]).openPopup();
         } 
     }
+    
 }
+
+function divClicked(id){
+
+    var data = document.getElementById("data");
+    if(globalCity == "Baku"){
+        data.innerHTML = Bakubeachnames[id] + "<br>" + Bakubeachinfo[id];
+
+    }
+    else{
+        data.innerHTML = Vanabeachnames[id] + "<br>" +Vanabeachinfo[id];
+    }
+
+    }
+    
+    
+
+  
+
 SetMap(split[1])
